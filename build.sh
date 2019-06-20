ec0aec347466b3e821fc5943f0f34623bb917bcc
Jyles Coad-Ward <jylescoadward@gmail.com>
2019-06-20 14:13:27 +0800

    1.1

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-electron-packager ./ --all "DARiOX Media" --arch=x64,arm64 --platform=darwin,linux,win32
+electron-packager ./ "DARiOX Media" --out=dist/osx --platform=darwin --arch=x64 --icon=image/logo.icns --osx-sign.identity="DARiOX" --overwrite
+electron-packager ./ "DARiOX Media" --out=dist/win --platform=win32 --arch=x64 --app-version=1.1 --icon=image/logo.ico --overwrite
+#zip -r dist/osx/DARiOXMediaApp-mac.zip dist/osx/DARiOX\ Media-darwin-x64/DARiOX\ Media.app/
+#zip -r dist/osx/DARiOXMediaApp-mac.zip dist/osx/DARiOX\ Media-win32-x64/
