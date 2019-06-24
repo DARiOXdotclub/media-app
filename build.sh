43baf56855cef345613ff492b60ec73b46de0a75
Jyles Coad-Ward <dariox.club@gmail.com>
2019-06-24 17:11:07 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-electron-packager ./ "DARiOX Media" --out=dist/osx --platform=darwin --arch=x64 --icon=image/logo.icns --osx-sign.identity="DARiOX" --overwrite
-electron-packager ./ "DARiOX Media" --out=dist/win --platform=win32 --arch=x64 --app-version=1.1 --icon=image/logo.ico --overwrite
-#zip -r dist/osx/DARiOXMediaApp-mac.zip dist/osx/DARiOX\ Media-darwin-x64/DARiOX\ Media.app/
-#zip -r dist/osx/DARiOXMediaApp-mac.zip dist/osx/DARiOX\ Media-win32-x64/
