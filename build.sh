69b680f0fd965603f8301dc8ff6a893bb16ca4fa
Jyles Coad-Ward <jylescoadward@gmail.com>
2019-06-24 17:45:32 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-electron-packager ./ "DARiOX Media" --out=dist/osx --platform=darwin --arch=x64 --icon=image/logo.icns --osx-sign.identity="DARiOX" --overwrite
-electron-packager ./ "DARiOX Media" --out=dist/win --platform=win32 --arch=x64 --app-version=1.1 --icon=image/logo.ico --overwrite
+electron-packager ./ "DARiOX Media" --out=dist/osx --platform=darwin --arch=x64 --icon=./image/circlogo.icns --osx-sign.identity="DARiOX" --overwrite
+electron-packager ./ "DARiOX Media" --out=dist/win --platform=win32 --arch=x64 --app-version=1.2.3 --icon=./image/circlogo.ico --overwrite
 #zip -r dist/osx/DARiOXMediaApp-mac.zip dist/osx/DARiOX\ Media-darwin-x64/DARiOX\ Media.app/
 #zip -r dist/osx/DARiOXMediaApp-mac.zip dist/osx/DARiOX\ Media-win32-x64/
